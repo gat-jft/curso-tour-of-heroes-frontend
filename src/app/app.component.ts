898c5bf0d85de1def5fd0505e326246ee48d521c
Jose Francisco <jfteixeira@yahoo.com.br>
2024-01-09 22:26:13 -0300

    Limpa tela inicial e coloca título

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -149,5 +149,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'curso-tour-of-heroes-frontend';
+  title = 'Tour of Heroes';
 }
