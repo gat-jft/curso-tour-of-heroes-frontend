8518c30a2d60905897d60d0afde0cfeb2da32899
Jose Francisco <jfteixeira@yahoo.com.br>
2024-02-20 21:06:56 -0300

    Lista de  herois

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,28 +1,23 @@
-import { Component, OnInit, Pipe } from '@angular/core'; /* (1) OnInit faz parte do ciclo de vida do Component, então logo após o Component ser criado, ele executa tudo que tiver dentro do OnInit(). (2) Pipe é desnecessário. Ele está no import do CommomModule */
+import { Component } from '@angular/core';
 import { Hero } from '../hero.model';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { HEROES } from '../mock-heroes';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [ FormsModule, CommonModule],
-
+  imports: [CommonModule, FormsModule],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.scss'
+  styleUrl: './heroes.component.scss',
 })
+export class HeroesComponent {
+  hero: Hero = { id: 1, name: 'Wolverine' };
 
-export class HeroesComponent implements OnInit  {
-  hero: Hero =
-   {
-    id: 1,
-    name: 'wolverine'
-   };
+  heroes = HEROES;
+  selectedHero?: Hero;
 
-  constructor() {} /* O construtor, a gente vai usar ele em vídeos futuros. Com ele a gente pode fazer Injeção de Dependência, iniciar uma variável, se for o caso. */
-
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }
